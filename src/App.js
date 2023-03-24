548d401a79232671d50024f87cda70086899c4f2
Nai <62731284+tobiastka@users.noreply.github.com>
2023-03-24 06:27:43 -0300

    Text's
    Se agrego la decepción del proyecto al lado

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ function App() {
           <Calculator />
         </div>
         <div className="app-description">
-          asd
+          <p className='description-title'>This is my super calculator</p>
+          <p className='description-subtitle'>Well maybe it's not that spectacular ԅ (≖‿≖ԅ)</p>
+          <p className='description-text'>This project was made with the intention of reviewing the concepts of component states through the use of hooks and a bit of logic.</p>
+          <p className='description-text'>The technology used in this project was mostly React, using vanilla CSS styles and MathJS to make the calculation of the operations a bit easier!</p>
         </div>
       </div>
     </div>
